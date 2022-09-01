33d7f2a16027302aca55bcfbe146a5d49689fd33
natefoxr <nate@foxr.xyz>
2022-08-31 20:24:18 -0700

    First commit and create multistream twitch view

---
+++ app.js
@@ -0,0 +1,41 @@
+var options1 = {
+    width: 750,
+    height: 415,
+    channel: "co1azo",
+    parent: ["localhost"],
+    muted: true,
+    autoplay: true,
+    };
+var player1 = new Twitch.Player("cole", options1);
+
+
+
+var options2 = {
+    width: 750,
+    height: 415,
+    channel: "nickmercs",
+    parent: ["localhost"],
+    muted: true,
+    autoplay: true,
+    };
+var player2 = new Twitch.Player("nick", options2);
+
+var options3 = {
+    width: 750,
+    height: 415,
+    channel: "aussieantics",
+    parent: ["localhost"],
+    muted: true,
+    autoplay: true,
+    };
+var player3 = new Twitch.Player("aussie", options3);
+
+var options4 = {
+    width: 750,
+    height: 415,
+    channel: "gabehaxx",
+    parent: ["localhost"],
+    muted: true,
+    autoplay: true,
+    };
+var player4 = new Twitch.Player("gabe", options4);
